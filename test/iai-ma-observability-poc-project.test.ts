bf941cb90135396081b353f2fe04029ca43c90ce
suds-sky <sudhakar.kirubakaran@sky.uk>
2022-08-12 10:57:38 +0100

    Initial commit

---
+++ test/iai-ma-observability-poc-project.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IaiMaObservabilityPocProject from '../lib/iai-ma-observability-poc-project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/iai-ma-observability-poc-project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IaiMaObservabilityPocProject.IaiMaObservabilityPocProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
